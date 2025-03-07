5f2f88b0ac8a883cd7cf3fcac185ee6d8a5d9334
Masakazu Takai <masakazu_takai@nagatsu-g.co.jp>
2025-03-07 14:28:37 +0900

    接続するデータベースをSQLSERVERに変更した。ログ出力機能も追加した。

--- app.py
+++ app.py
@@ -6,19 +6,36 @@ import json
 import base64
 from datetime import date, datetime
 import pytz
-import sqlite3
+# import sqlite3    # SQLiteへの接続は廃止
+import logging
+import pyodbc   # SQL Serverへの接続に変更
 
 dotenv.load_dotenv()
 
 USER_ID = os.getenv('USER_ID')
 API_KEY = os.getenv('API_KEY')
 API_URL = os.getenv('API_URL')
+
+DATABASE_SERVER = os.getenv('DATABASE_SERVER')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+
+DB_CONNECTION_STRING = f"DRIVER={{SQL Server}};SERVER={DATABASE_SERVER};DATABASE={DATABASE_NAME};UID={DATABASE_USER};PWD={DATABASE_PASSWORD}"
 config = {}
+
+logging.basicConfig(
+    filename='check_approved_requests.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 with open("config.yml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
-DATABASE_NAME = "purchase_requisitions.db"  # データベースファイル名
-TABLE_NAME = "aproved_purchase_requests" #テーブル名
+# DATABASE_NAME = "purchase_requisitions.db"  # データベースファイル名
+TABLE_NAME = config["table_name"]
 
 def main():
     """
@@ -31,8 +48,12 @@ def main():
         newly_added_count = save_documents_to_db(parsed_data)   # データベースに保存
         print(f"申請文書データをデータベース({DATABASE_NAME})の{TABLE_NAME}テーブルに保存しました。") #テーブル名出力
         print(f"新規登録件数：{newly_added_count}件")
+        if newly_added_count > 0:
+            msg = f"申請文書データをデータベース({DATABASE_NAME})の{TABLE_NAME}テーブルに保存しました。新規登録件数：{newly_added_count}件"
+            logging.info(msg=msg)
     else:
         print("検索結果が取得できませんでした。")
+        logging.ERROR("申請文書データの検索結果が取得できませんでした。")
 
 
 def get_data(form_id=40):
@@ -137,24 +158,33 @@ def parse_documents_list(data: dict, form_id=40) -> list:
     return documents_list   # リストを返す
 
 
-def create_table(conn):
+def create_table(cursor):
     """
     データベースにテーブルを作成する関数。
     """
-    cursor = conn.cursor()
-    cursor.execute(f"""
-        CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-            document_id INTEGER PRIMARY KEY,
-            document_number TEXT,
-            title TEXT,
-            request_user TEXT,
-            request_group TEXT,
-            end_date TEXT,
-            form_id INTEGER,
-            request_factory TEXT
-        )
-    """)
-    conn.commit()
+    try:
+        # テーブルが既に存在するかどうかを確認しながらテーブル作成
+        cursor.execute(f"""
+            IF OBJECT_ID('{TABLE_NAME}', 'U') IS NULL
+            CREATE TABLE {TABLE_NAME} (
+                document_id INT not null,
+                document_number nvarchar(30),
+                title nvarchar(100),
+                request_user nvarchar(40),
+                request_group nvarchar(20),
+                request_factory nvarchar(20) default '',                
+                form_id INT default 0,
+                end_date datetime,
+                primary key (document_id)
+            )
+        """)
+        return True
+    except Exception as e:
+        print(f"テーブルの作成に失敗しました: {e}")
+        logging.error(f"{TABLE_NAME}テーブルの作成に失敗しました: {e}")
+        return False
+
+    
 
 
 def save_documents_to_db(documents):
@@ -165,34 +195,57 @@ def save_documents_to_db(documents):
     """
     conn = None
     newly_added_count = 0
+
     try:
-        conn = sqlite3.connect(DATABASE_NAME)
-        create_table(conn)
+        # Microsoft SQL Server接続文字列
+        conn_str = DB_CONNECTION_STRING
+        conn = pyodbc.connect(conn_str)
         cursor = conn.cursor()
+        # データベース接続
+        if create_table(cursor):
+                    # INSERT文を変更
+        # 現在の行数を取得
+            cursor.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}")
+            current_count = cursor.fetchone()[0]
 
-        for document in documents:
-            cursor.execute(f"""
-                INSERT OR IGNORE INTO {TABLE_NAME} (
-                    document_id, document_number, title, request_user,
-                    request_group, end_date, form_id, request_factory
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            """, (
-                document["document_id"],
-                document["document_number"],
-                document["title"],
-                document["request_user"],
-                document["request_group"],
-                document["end_date"],
-                document["form_id"],
-                document.get("request_factory")
-            ))
-            newly_added_count += cursor.rowcount #挿入された行数を加算
-        conn.commit()
+            for document in documents:
+                cursor.execute(f"""
+                    IF NOT EXISTS (
+                        SELECT 1
+                        FROM {TABLE_NAME}
+                        WHERE document_id = ?
+                    )
+                    INSERT INTO {TABLE_NAME} (
+                        document_id, document_number, title, request_user,
+                        request_group, end_date, form_id, request_factory
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """, (
+                    document["document_id"],
+                    document["document_id"],
+                    document["document_number"],
+                    document["title"],
+                    document["request_user"],
+                    document["request_group"],
+                    document["end_date"],
+                    document["form_id"],
+                    document.get("request_factory", ""),
+                ))
+            
+            # INSERT後の行数を取得
+            cursor.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}")
+            new_count = cursor.fetchone()[0]
 
-    except sqlite3.Error as e:
+            # 新規登録件数を計算
+            newly_added_count = new_count - current_count
+
+            conn.commit()
+
+    except pyodbc.Error as e:
         print(f"データベースエラー: {e}")
+        logging.error(f"データベースエラー: {e}")
     finally:
         if conn:
+            cursor.close()
             conn.close()
     return newly_added_count # 新規登録件数を返す
 
